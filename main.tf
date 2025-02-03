3c0bd83c3e019cc45d226872027ace278245a204
Prabal-saxena <prabalsaxena98@gmail.com>
2025-02-03 18:01:55 -0500

    ci-cd & GKE SA creation with output

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+resource "google_service_account" "gke-sa" {
+  account_id   = "gke-sa"
+  display_name = "SA for Kubernetes"
+  project = "spheric-base-448422-q9"
+  create_ignore_already_exists = true
+}
+
+resource "google_service_account_key" "gke_sa_key" {
+  service_account_id = google_service_account.gke-sa.id
+}
+
+resource "google_storage_bucket_object" "service_account_key" {
+  name   = "service_account_key.json"  # File name to store the credentials in GCS
+  bucket = "onlineliquorservices_bucket"    # Your GCS bucket name
+  source = google_service_account_key.gke_sa_key.private_key_path  # Path to the generated key
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "onlineliquorservices_bucket"
+    prefix  = "terraform/serviceaccount/tfstate"
+  }
+}
