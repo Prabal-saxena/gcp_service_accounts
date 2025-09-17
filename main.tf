ee3c9391cef016fdbfccdb1232788f1c2b77f9ec
Prabal-saxena <prabalsaxena98@gmail.com>
2025-09-17 17:59:50 -0400

    GSA for Product, Order & User Microservice

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ resource "google_service_account" "gke-service-sa" {
   create_ignore_already_exists = true
 }
 
-resource "google_service_account" "ci-cd-sa" {
-  account_id   = "ci-cd-sa"
-  display_name = "SA for Ci CD"
-  project = "noble-linker-471623-s6"
-  create_ignore_already_exists = true
-}
-
 resource "google_service_account_key" "gke_sa_key" {
   service_account_id = google_service_account.gke-service-sa.id
 }
@@ -29,6 +22,34 @@ resource "google_storage_bucket_object" "service_account_key" {
   source = local_file.service_account_key_file.filename  # Using the local variable
 
   depends_on = [local_file.service_account_key_file] # Ensure the file is created before uploading
+}
+
+resource "google_service_account" "ci-cd-sa" {
+  account_id   = "ci-cd-sa"
+  display_name = "SA for Ci CD"
+  project = "noble-linker-471623-s6"
+  create_ignore_already_exists = true
+}
+
+resource "google_service_account" "product-service-sa" {
+  account_id   = "product-service-sa"
+  display_name = "SA for Product Microservice"
+  project = "noble-linker-471623-s6"
+  create_ignore_already_exists = true
+}
+
+resource "google_service_account" "order-service-sa" {
+  account_id   = "order-service-sa"
+  display_name = "SA for Order Microservice"
+  project = "noble-linker-471623-s6"
+  create_ignore_already_exists = true
+}
+
+resource "google_service_account" "user-service-sa" {
+  account_id   = "user-service-sa"
+  display_name = "SA for User Service"
+  project = "noble-linker-471623-s6"
+  create_ignore_already_exists = true
 }
 
 terraform {
